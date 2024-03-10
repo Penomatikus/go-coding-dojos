ef0c05762bdb66f0ae448fec775f3be582488e7a
Stefan Jagdmann <sjagdmann10@googlemail.com>
2024-03-10 22:54:58 +0100

    adds simple equasion token package

--- calculator/internal/rpn/token/token.go
+++ calculator/internal/rpn/token/token.go
@@ -1,10 +1,19 @@
 package token
 
 const (
-	Addition           = '+'
-	Substaction        = '-'
-	Multiplication     = '*'
-	Division           = '/'
-	OpeningParenthesis = '('
-	ClosingParenthesis = ')'
+	Addition           = "+"
+	Substaction        = "-"
+	Multiplication     = "*"
+	Division           = "/"
+	OpeningParenthesis = "("
+	ClosingParenthesis = ")"
 )
+
+func IsOperator(s string) bool {
+	switch s {
+	case Addition, Substaction, Division, Multiplication:
+		return true
+	default:
+		return false
+	}
+}
