4b339efb4c73a1fb949a54a05ce9305925ed0875
Stefan Jagdmann <sjagdmann10@googlemail.com>
2024-03-10 18:00:34 +0100

    Adds tokens

---
+++ calculator/internal/rpn/token/token.go
@@ -0,0 +1,10 @@
+package token
+
+const (
+	Addition           = '+'
+	Substaction        = '-'
+	Multiplication     = '*'
+	Division           = '/'
+	OpeningParenthesis = '('
+	ClosingParenthesis = ')'
+)
